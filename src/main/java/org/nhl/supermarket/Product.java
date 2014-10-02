ffca33d778ca6f56001da3723e822bf316d10a7d
Rubykuby <rubykuby@gmail.com>
2014-10-02 16:13:13 +0200

    Changed Product to only have getters

--- src/main/java/org/nhl/supermarket/Product.java
+++ src/main/java/org/nhl/supermarket/Product.java
@@ -20,23 +20,11 @@ public class Product {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public BigDecimal getPrice() {
         return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
     }
 }
