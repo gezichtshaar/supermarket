87fde015536110588f312c744f4fd0b42a1bf183
Rubykuby <rubykuby@gmail.com>
2014-10-02 16:07:51 +0200

    Product implemented

--- src/main/java/org/nhl/supermarket/Product.java
+++ src/main/java/org/nhl/supermarket/Product.java
@@ -6,6 +6,37 @@ import java.math.BigDecimal;
  * Created by ruben on 02/10/14.
  */
 public class Product {
+    private int id;
     private String name;
     private BigDecimal price;
+
+    public Product(int id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
