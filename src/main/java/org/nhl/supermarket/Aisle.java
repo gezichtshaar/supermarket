99d7cb7de7723f9303dabcfb7fd58eaa1f484d51
Rubykuby <rubykuby@gmail.com>
2014-10-06 13:42:55 +0200

    Added exception instead of null return

--- src/main/java/org/nhl/supermarket/Aisle.java
+++ src/main/java/org/nhl/supermarket/Aisle.java
@@ -1,10 +1,7 @@
 package org.nhl.supermarket;
 
 import java.math.BigDecimal;
-import java.util.Stack;
-import java.util.Map;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Created by ruben on 02/10/14.
@@ -22,7 +19,7 @@ public class Aisle implements BuyZone, Task {
                 return shelf.takeProduct();
             }
         }
-        return null;
+        throw new IllegalArgumentException();
     }
 
     @Override
