4f9571c332dfaa2bf357e5663b78fd77efd6a973
remynaps <remysije@gmail.com>
2014-10-06 10:54:47 +0200

    de customer klasses toegevoegd en de employee

---
+++ src/main/java/org/nhl/supermarket/DiscountAisle.java
@@ -0,0 +1,10 @@
+package org.nhl.supermarket;
+
+/**
+ * Created by remy on 06/10/14.
+ */
+public class DiscountAisle extends Aisle {
+    public DiscountAisle() {
+
+    }
+}
---
+++ src/main/java/org/nhl/supermarket/StudentCustomer.java
@@ -0,0 +1,10 @@
+package org.nhl.supermarket;
+
+/**
+ * Created by remy on 06/10/14.
+ */
+public class StudentCustomer extends Customer {
+    public void act(Supermarket supermarket){
+
+    }
+}
