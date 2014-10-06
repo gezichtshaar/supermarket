548f45568fa3f91b660c9fd32bc409239fed7f68
Rubykuby <rubykuby@gmail.com>
2014-10-06 10:41:51 +0200

    Added Task interface to Aisle

--- src/main/java/org/nhl/supermarket/Aisle.java
+++ src/main/java/org/nhl/supermarket/Aisle.java
@@ -5,10 +5,14 @@ import java.math.BigDecimal;
 /**
  * Created by ruben on 02/10/14.
  */
-public class Aisle implements BuyZone {
+public class Aisle implements BuyZone, Task {
     private String name;
     private Product[] products;
     private String[] productNames;
+
+    public void update(Supermarket supermarket) {
+        // Logic
+    }
 
     public Product takeProduct(String productName) {
         // dummy return value
