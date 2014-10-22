daf616552009e96d4dfefd4ba1bb7d4730310ee0
Rubykuby <rubykuby@gmail.com>
2014-10-22 10:14:23 +0200

    findMissingProducts() implemented

--- src/main/java/org/nhl/supermarket/actors/Customer.java
+++ src/main/java/org/nhl/supermarket/actors/Customer.java
@@ -6,8 +6,10 @@ import org.nhl.supermarket.interfaces.Person;
 import org.nhl.supermarket.models.Product;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by ruben on 02/10/14.
@@ -15,11 +17,14 @@ import java.util.List;
 public abstract class Customer implements Person {
     protected int indexPosition = 0;
     protected List<Product> shoppingCart;
-    protected HashMap<Integer, Integer> desiredProductIds;
+    protected Map<Integer, Integer> desiredProductIds;
     protected BigDecimal balance;
 
     public Customer(BigDecimal balance) {
         this.balance = balance;
+        this.shoppingCart = new ArrayList<Product>();
+        // Needs implementation.
+        this.desiredProductIds = new HashMap<Integer, Integer>();
     }
 
     public List<Product> getShoppingCart() {
@@ -28,6 +33,16 @@ public abstract class Customer implements Person {
 
     public BigDecimal getBalance() {
         return balance;
+    }
+
+    public List<Integer> findMissingProducts() {
+        List<Integer> missingProducts = new ArrayList<Integer>();
+        for (int id : desiredProductIds.keySet()) {
+            if (!shoppingCart.contains(id)) {
+                missingProducts.add(id);
+            }
+        }
+        return missingProducts;
     }
 
     public void subtractFromBalance(BigDecimal amount) {
